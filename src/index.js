2e06c3f17e7cbb99924c18cd6e7b2568f416c5f7
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-01-02 21:13:01 +0200

    add initialize Swiper

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-// import Swiper JS
 import Swiper from 'swiper';
+//import 'swiper/css';
+
+const pageSlider = new Swiper('.slider', {
+  speed: 1000,
+  scrollbar: {
+    el: '.slider__scrollbar',
+    draggable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      centeredSlides: false,
+    },
+    992: {
+      slidesPerView: 2,
+      centeredSlides: true,
+    },
+  },
+});
