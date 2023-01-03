2838d2d4519e0b525e9e5947b69a5f0d85f45612
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-01-03 21:05:24 +0200

    add render markup, controller, effect fade, loop and speed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import Swiper from 'swiper';
-//import 'swiper/css';
+import Swiper, { Scrollbar, Controller, EffectFade } from 'swiper';
 
 const pageSlider = new Swiper('.slider', {
+  modules: [Scrollbar, Controller],
+  loop: true,
+
   speed: 1000,
   scrollbar: {
     el: '.slider__scrollbar',
@@ -18,3 +20,64 @@ const pageSlider = new Swiper('.slider', {
     },
   },
 });
+
+const page = document.querySelector('.page');
+const images = document.querySelectorAll('.slide__picture');
+
+if (images.length) {
+  let backgroundSlides = ``;
+  let textSlides = ``;
+
+  images.forEach(image => {
+    backgroundSlides += ` 
+    <div class="background__slide swiper-slide">
+      <div class="background__image">
+        <img src="${image.getAttribute('src')}" alt="${image.alt}" />
+      </div>
+    </div>
+    `;
+
+    textSlides += `
+    <div class="text__slide swiper-slide">
+      <span>${image.dataset.title ? image.dataset.title : ''}</span>
+    </div>
+      `;
+  });
+
+  const background = `
+    <div class="background swiper">
+    <div class="background__wrapper swiper-wrapper">
+    ${backgroundSlides}
+     </div>
+     </div>
+    `;
+
+  const text = `
+    <div class="text swiper">
+    <div class="text__wrapper swiper-wrapper">
+    ${textSlides}
+     </div>
+     </div>
+    `;
+
+  page.insertAdjacentHTML('afterbegin', background);
+  page.insertAdjacentHTML('beforeend', text);
+
+  const pageBgSlider = new Swiper('.background', {
+    modules: [Scrollbar, Controller],
+    speed: 500,
+  });
+
+  const pageTextSlider = new Swiper('.text', {
+    modules: [Scrollbar, Controller, EffectFade],
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true,
+    },
+    speed: 1000,
+  });
+
+  // controller
+  pageSlider.controller.control = pageBgSlider;
+  pageBgSlider.controller.control = pageTextSlider;
+}
